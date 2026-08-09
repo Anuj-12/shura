46c219e5d18fa69b150d2d627668c204fcd1a7f9
Anuj-12 <anujtjs@gmail.com>
2026-08-09 12:39:55 +0530

    Removed debugging code

--- src/stt.py
+++ src/stt.py
@@ -45,7 +45,7 @@ def transcribe() -> str:
     
     # Make all the individual signals a single signal
     audio = np.concatenate(capture_buffer, axis=0).squeeze()
-    save_wav("debug.wav", audio)
+    #save_wav("debug.wav", audio)
     segments, info = model.transcribe(audio)
 
     text = ""
